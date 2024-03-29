442f5c0f41104fc8c43755ee23a0daae2678097b
Yuri Drigin <yuri.drigin@icloud.com>
2024-03-29 12:40:58 +0400

    Finished

--- client/index.js
+++ client/index.js
@@ -5,13 +5,26 @@ const MerkleTree = require('../utils/MerkleTree');
 const serverUrl = 'http://localhost:1225';
 
 async function main() {
-  // TODO: how do we prove to the server we're on the nice list? 
+  
+  if (!process.argv.count > 2) {
+    console.log("Please add name to check as argument:");
+    console.log("node client/index.js Your Name");
+    return;
+  }
 
-  const { data: gift } = await axios.post(`${serverUrl}/gift`, {
-    // TODO: add request body parameters here!
+  const name = process.argv.slice(2).join(" ");
+  console.log(`Checking ${name} for ...`);
+
+  const merkleTree = new MerkleTree(niceList);
+  const index = niceList.findIndex(n => n === name);
+  const proof = merkleTree.getProof(index);
+
+  const { data: result } = await axios.post(`${serverUrl}/gift`, {
+    name: name,
+    proof: proof
   });
 
-  console.log({ gift });
+  console.log(result);
 }
 
 main();
